dccd0f73fe21d541ba76cb542885e3162b4be8a6
bj <bj@agilethinking.org>
2015-06-18 23:05:00 +0100

    Fully working example with try / except clauses

--- q2/q2.py
+++ q2/q2.py
@@ -3,7 +3,7 @@ __author__ = '30070_VS'
 """
 Q2 Web Front-End Test
 Automate the following functional test using Selenium:
-1. .
+1. Navigate to the Wikipedia home page, http://www.wikipedia.org/.
 2. Search for a given string in English:
 (a) Type in a string given as parameter in the search input field.
 (b) Select English as the search language.
@@ -18,7 +18,7 @@ link to the version in English.
 """
 
 from selenium import webdriver
-# from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support import expected_conditions
@@ -28,33 +28,57 @@ start_page = "http://www.wikipedia.org/"
 search_field_name = "search"
 search_string = "London"
 search_lang = "Deutsch"
+# search_lang = "wiggle"
 
 # Create a new instance of the Firefox driver.
 driver = webdriver.Firefox()
 
-# Navigate to the Wikipedia home page, http://www.wikipedia.org/.
-driver.get(start_page)
-
-# the page is ajaxy so the title is originally this:
-print(driver.title)
-
-# 2. Search for a given string in English:
-# (a) Type in a string given as parameter in the search input field.
-inputElement = driver.find_element_by_name(search_field_name)
-# type in the search
-inputElement.send_keys(search_string)
-# (b) Select English as the search language.
-select = Select(driver.find_element_by_name('language'))
-select.select_by_visible_text("English")
-
-# submit the form (although google automatically searches now without submitting)
-driver.find_element_by_name("go").click()
-
 try:
-    # we have to wait for the page to refresh, the last thing that seems to be updated is the title
-    WebDriverWait(driver, 10).until(expected_conditions.title_contains(search_string))
+    # 1. Navigate to the Wikipedia home page, http://www.wikipedia.org/.
+    driver.get(start_page)
+    try:
+        element = WebDriverWait(driver, 10).until(expected_conditions.title_contains("Wikipedia"))
+    except:
+        print("The %s page failed to load! FAIL" % start_page)
+        raise
 
-    print(driver.title)
+
+    # 2. Search for a given string in English:
+    # (a) Type in a string given as parameter in the search input field.
+    inputElement = driver.find_element_by_name(search_field_name)
+    # type in the search
+    inputElement.send_keys(search_string)
+    # (b) Select English as the search language.
+    select = Select(driver.find_element_by_name('language'))
+    select.select_by_visible_text("English")
+    # (c) Click the search button.
+    driver.find_element_by_name("go").click()
+
+    # 3. Validate that the first heading of the search results page matches the search string (ignoring case).
+    if search_string.lower() not in driver.find_element_by_xpath("/html/body//h1").text.lower():
+        raise AssertionError("Search string (%s) not found in first header" % search_string)
+
+    # 4. Verify that the search results page is available in a language given as parameter.
+    try:
+        new_lang_page = driver.find_element_by_xpath("//div[@id='p-lang']").find_element_by_partial_link_text(search_lang)
+    except NoSuchElementException:
+        print("Page not available in %s. FAIL" % search_lang)
+        raise
+    else:
+        print("Page is available in %s. PASS" % search_lang)
+
+    # 5. Navigate to the search results page in that language.
+    new_lang_page.click()
+
+    # 6. Validate that the search results page in the new language includes a link to the version in English
+    en_lang = "English"
+    try:
+        new_lang_page = driver.find_element_by_xpath("//div[@id='p-lang']").find_element_by_partial_link_text(en_lang)
+    except NoSuchElementException:
+        print("Page not available in %s. FAIL" % en_lang)
+        raise
+    else:
+        print("Page is available in %s. PASS" % en_lang)
 
 finally:
     driver.quit()
