a171bff61d2426dbb4526f04ad4d3a0cd48f7426
Bernard 'BJ' Jauregui <bj@agilethinking.org>
2015-06-18 17:33:55 +0100

    Q2 Changed to work with Wikipedia

--- q2/q2.py
+++ q2/q2.py
@@ -3,7 +3,7 @@ __author__ = '30070_VS'
 """
 Q2 Web Front-End Test
 Automate the following functional test using Selenium:
-1. Navigate to the Wikipedia home page, http://www.wikipedia.org/.
+1. .
 2. Search for a given string in English:
 (a) Type in a string given as parameter in the search input field.
 (b) Select English as the search language.
@@ -18,33 +18,36 @@ link to the version in English.
 """
 
 from selenium import webdriver
-from selenium.common.exceptions import TimeoutException
+# from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support import expected_conditions
 
-# Create a new instance of the Firefox driver
+# Test Data
+start_page = "http://www.wikipedia.org/"
+search_field_name = "search"
+search_string = "London"
+
+# Create a new instance of the Firefox driver.
 driver = webdriver.Firefox()
 
-# go to the google home page
-driver.get("http://www.google.com")
+# Navigate to the Wikipedia home page, http://www.wikipedia.org/.
+driver.get(start_page)
 
 # the page is ajaxy so the title is originally this:
 print(driver.title)
 
-# find the element that's name attribute is q (the google search box)
-inputElement = driver.find_element_by_name("q")
-
+# 2. Search for a given string in English:
+# (a) Type in a string given as parameter in the search input field.
+inputElement = driver.find_element_by_name(search_field_name)
 # type in the search
-inputElement.send_keys("cheese!")
-
+inputElement.send_keys(search_string)
 # submit the form (although google automatically searches now without submitting)
 inputElement.submit()
 
 try:
     # we have to wait for the page to refresh, the last thing that seems to be updated is the title
-    WebDriverWait(driver, 10).until(EC.title_contains("cheese!"))
+    WebDriverWait(driver, 10).until(expected_conditions.title_contains(search_string))
 
-    # You should see "cheese! - Google Search"
     print(driver.title)
 
 finally:
