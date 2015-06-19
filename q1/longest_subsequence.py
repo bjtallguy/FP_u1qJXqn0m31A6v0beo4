5e519c5ff9218cba44362662332c739911411b9c
Bernard 'BJ' Jauregui <bj@agilethinking.org>
2015-06-19 14:50:40 +0100

    Better comments and organisation. Added directory for unit tests

--- q1/longest_subsequence.py
+++ q1/longest_subsequence.py
@@ -1,20 +1,15 @@
 __author__ = 'bj'
 
-from random import randint
 
-test1 = [1, 4, 1, 4, 2, 1, 3, 5, 6, 2, 3, 7]
-test2 = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
-test3 = [2, 7, 1, 8, 2, 8, 1]
-test4 = []
-test5 = [3]
-test6 = [2, 2, 2, 2, 2, 2, 2]
-test7 = ["abc"]
-test8 = ["a", "b", "c", "b"]
+def find_longest_inc_subsequence(seq):
+    """Solution for Q1
+    Given an unordered array of integers of length N > 0, calculate the length of
+    the longest ordered (ascending from left [lower index] to right [higher index])
+    sub-sequence within the array.
 
-test_suite = [test1, test2, test3, test4, test5, test6, test7, test8]
+    * Recursive, functional and binary-search solutions have already been rejected.
+    """
 
-
-def fll_non_recursive(seq):
     best_len = 0
     curr_len = 0
     if seq:
@@ -32,10 +27,26 @@ def fll_non_recursive(seq):
     return best_len
 
 
-for test in test_suite:
-    print(fll_non_recursive(test), " : ", test)
+if __name__ == '__main__':
+    """Basic demonstration - Note: See tests directory for unit tests
+    """
+    from random import randint
 
-test = []
-for i in range(10**6):
-    test.append(randint(0, 1000))
-print(fll_non_recursive(test), " : ", len(test), " integer list")
+    test1 = [1, 4, 1, 4, 2, 1, 3, 5, 6, 2, 3, 7]
+    test2 = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5]
+    test3 = [2, 7, 1, 8, 2, 8, 1]
+    test4 = []
+    test5 = [3]
+    test6 = [2, 2, 2, 2, 2, 2, 2]
+    test7 = ["abc"]
+    test8 = ["a", "b", "c", "b"]
+
+    test_suite = [test1, test2, test3, test4, test5, test6, test7, test8]
+
+    for test in test_suite:
+        print(find_longest_inc_subsequence(test), " : ", test)
+
+    test = []
+    for i in range(10**6):
+        test.append(randint(0, 1000))
+    print(find_longest_inc_subsequence(test), " : ", len(test), " integer list")
