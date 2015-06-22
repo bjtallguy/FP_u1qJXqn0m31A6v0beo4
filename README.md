85a3ee5041fba0a24912422411d8350798ffb816
bj <bj@agilethinking.org>
2015-06-22 01:10:23 +0100

    Final tidy before publishing

--- README.md
+++ README.md
@@ -6,8 +6,11 @@ This can be run using 'python -m nose' from the q1/tests directory, or using the
 
 *Q2)* Developed in python, and as a Gherkin feature, with steps implemented in python
 
-This can be run using 'behave' from within the features directory, or using the test runner in PyCharm.
+This can be run using 'behave' from within the q2/tests directory, or using the test runner in PyCharm.
 
-*Q3)* Developed in python using pySimpleSoap to set up my client. 
+*Q3)* Developed in python using pySimpleSoap to set up my client and demonstrating three different techniques 
+for comparing XML element trees. (None are perfect, but each takes a different approach and has its own 
+strengths and weaknesses.)
 
-To compare the XML response with a test string, I created a simple ordered serialiser.  
+This can be run with python main.py from the q2 directory - Edit the test_soap_call function to select which 
+tests to use.
--- q3/main.py
+++ q3/main.py
@@ -1,7 +1,19 @@
 __author__ = 'bj'
 
+"""Test a SOAP call function by comparing the response to a string.
+
+There are two main functions: The SOAP call: get_uk_location_by_postcode() and the test framework: test_soap_call
+
+Actual XML Element Tree comparison is difficult, so I have presented three different approaches:
+    Serialisation using a common parser - serialise_and_compare()
+    Compare all nodes regardless of order - compare_xpaths()
+    Show what is actually different using two tree analysis algorithms - xmldiff_compare()
+Which to use will depend on the context.
+"""
+
 import xml.etree.ElementTree as ET
 from pysimplesoap.client import SoapClient
+from pyxmldiff.xmldiff import xmlDiff
 
 
 def get_uk_location_by_postcode(county):
@@ -11,8 +23,8 @@ def get_uk_location_by_postcode(county):
         results = client.GetUKLocationByCounty(county)
     except:
         # save sent and received messages for debugging:
-        open("request.xml", "w").write(client.xml_request)
-        open("response.xml", "w").write(client.xml_response)
+        open("request.xml", "w").write(str(client.xml_request))
+        open("response.xml", "w").write(str(client.xml_response))
         raise
 
     return results['GetUKLocationByCountyResult']
@@ -26,102 +38,47 @@ def get_file_as_string(filename):
     return str_data
 
 
-def compare_element_trees(a, b):
-    """
-    Walk the entire XML element tree, in alphabetical order to elements, tags, text and tails, comparing every node.
-    :param a: ElementTree
-    :param b: ElementTree
-    :return: bool
-    """
+def serialise_and_compare(a, b):
+    return a.tostring() == b.tostring()
 
-    if a.tag != b.tag:
+
+def xmldiff_compare(a, b):
+    results = xmlDiff(a, b)
+    if results:
+        print(results)
         return False
-
-    if (a.text and b.text) and (a.text != b.text):
-            return False
-    elif b.text:
-        return False
-
-    if (a.tail and b.tail) and (a.tail != b.tail):
-            return False
-    elif b.tail:
-        return False
-
-    # Attributes are retirieved as a list of (name, value) pairs.
-    if sorted(a.items()) != sorted(b.items()):
-        return False
-
-
-def xml_compare(x1, x2, reporter=None):
-    if x1.tag != x2.tag:
-        if reporter:
-            reporter('Tags do not match: %s and %s' % (x1.tag, x2.tag))
-        return False
-    for name, value in x1.attrib.items():
-        if x2.attrib.get(name) != value:
-            if reporter:
-                reporter('Attributes do not match: %s=%r, %s=%r'
-                         % (name, value, name, x2.attrib.get(name)))
-            return False
-    for name in x2.attrib.keys():
-        if name not in x1.attrib:
-            if reporter:
-                reporter('x2 has an attribute x1 is missing: %s'
-                         % name)
-            return False
-    if not text_compare(x1.text, x2.text):
-        if reporter:
-            reporter('text: %r != %r' % (x1.text, x2.text))
-        return False
-    if not text_compare(x1.tail, x2.tail):
-        if reporter:
-            reporter('tail: %r != %r' % (x1.tail, x2.tail))
-        return False
-
-    cl1 = x1.getchildren()
-    cl2 = x2.getchildren()
-
-    if len(cl1) != len(cl2):
-        if reporter:
-            reporter('children length differs, %i != %i'
-                     % (len(cl1), len(cl2)))
-        return False
-
-    i = 0
-    for c1, c2 in zip(cl1, cl2):
-        i += 1
-        if not xml_compare(c1, c2, reporter=reporter):
-            if reporter:
-                reporter('children %i do not match: %s'
-                         % (i, c1.tag))
-            return False
-
     return True
 
 
-def text_compare(t1, t2):
-    if not t1 and not t2:
-        return True
-    if t1 == '*' or t2 == '*':
-        return True
-    return (t1 or '').strip() == (t2 or '').strip()
+def compare_xpaths(a, b):
+    return sorted(list_of_xpaths(a)) == sorted(list_of_xpaths(b))
+
+
+def list_of_xpaths(root):
+    result = ['/' + root.tag + '/' + (root.text or '').strip()]
+    for child in root.getchildren():
+        for xpath in list_of_xpaths(child):
+            result.append('/' + root.tag + xpath)
+    return result
 
 
 def test_soap_call(soap_call, expected_response, *args):
     """Test a SOAP call function by comparing the response to a string.
-    Actual XML Element Tree comparison is difficult, so I have parsed both the fresh response and recorded response
-    using an XML parser known to maintain order (Python's XParse); then compared the serialised output of each.
+    Actual XML Element Tree comparison is difficult, so I have presented three different approaches:
+        Serialisation using a common parser - serialise_and_compare
+        Compare all nodes regardless of order - compare_xpaths
+        XMLDiff - to show what is actually different using two tree analysis algorithms
+    Which to use will depend on the context.
     :param soap_call type:function
     :param expected_response type:str
     :param args type:[str]
     """
     expected_et = ET.fromstring(expected_response)
     actual_et = ET.fromstring(soap_call(*args))
-    if xml_compare(expected_et, actual_et, reporter=print):
+    if xmldiff_compare(expected_et, actual_et):
         print("Given string forms a matching XML element tree. PASS")
     else:
         print("Given string does not form a matching XML element tree! FAIL")
-        print(ET.tostring(actual_et), "\n", ET.tostring(expected_et))
 
 
 if __name__ == '__main__':
